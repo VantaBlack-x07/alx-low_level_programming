192584b6b275601968419b5ab4291a74df1f7167
ASSEM <asmhfny22@gmail.com>
2023-03-22 15:35:41 -0700

    s

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* 
+* Description: _putchar writes the character c
+*
+* Return: 0 (success)
+*	 -1 (error)
+*/
+
+int _putchar(char c)
+
+	return (write(1, &c, 1));
