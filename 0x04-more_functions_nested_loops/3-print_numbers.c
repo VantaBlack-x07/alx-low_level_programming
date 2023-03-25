88c09e495e0a3f9f3b3d7e9df01efc7282d6c352
ASSEM <asmhfny22@gmail.com>
2023-03-24 17:37:51 -0700

    d

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,11 +11,10 @@
 void print_numbers(void)
 {
 	num = 0;
-
-	while (num >= 0 && num <= 9)
-	{
-		_putchar(num + 48);
-		num++;
-		_putchar('\n');
+	do {
+	_putchar(num + 48);
+	num++;
 	}
+	while (num >= 0 && num <= 9)
+		_putchar('\n');
 }
