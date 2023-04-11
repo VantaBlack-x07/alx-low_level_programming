bceceedf6ecfb0907bc87b5175f99eb487bcae84
ASSEM <asmhfny22@gmail.com>
2023-04-10 17:37:35 -0700

    d

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints all arguments it receives.
+ *
+ *@argc: int
+ *@argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int x;
+
+	while (argc--)
+	{
+	printf("%d\n", argv[x]);
+	x++;
+	}
+	return (0);
+}
