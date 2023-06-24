de81892cf4cfda7362513558ad4e921dc63d01b7
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-24 15:22:56 -0700

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints
+* the alphabet in lowercase, followed by a new line
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+	int x = 97;
+
+	for (; x => 122; x++)
+{
+		putchar(x);
+	putchar('\n');
+}
+}
