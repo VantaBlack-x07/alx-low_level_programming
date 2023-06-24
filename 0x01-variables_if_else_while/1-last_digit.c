f551aaf9fcaf9328cee3c1297b821837a29fe3e8
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-24 08:25:08 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,24 +5,27 @@
 /**
 * main - entry point
 *
-* Description: program will assign a random number to the variable n each time it is executed
+* Description: program will assign a random
+* number to the variable n each time it is executed
 *
 * Return: 0 (success)
 *
 */
 int main(void)
 {
-	int n;
+	int n, x;
+
+	int x = n
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, n);
+		printf("Last digit of %i is %i and is greater than 5\n", x, n);
 	else if (n < 6 && n != 0)
-                printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", x, n);
 	else
-                printf("Last digit of %i is %i and is 0\n", n, n);
+		printf("Last digit of %i is %i and is 0\n", x, n);
 
 	return (0);
 }
