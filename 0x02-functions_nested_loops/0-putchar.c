ad211381a835e2c76266979944b099ea17a22eec
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-25 13:15:51 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,16 @@
 *
 */
 
-int _putchar(char c)
+int main(void)
 {
-	_putchar("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
 	return (0);
 }
