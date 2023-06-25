6259830c205f406d27448597694f442f35e465fe
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-25 06:18:36 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints all single digit numbers
+* of base 10 starting from 0, followed by a new line
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+{
+		putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
