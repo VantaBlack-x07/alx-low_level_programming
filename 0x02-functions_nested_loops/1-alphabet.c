6b3ea375f46ecc9858a1a867f433bd194d33a69b
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-25 13:25:40 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - program that prints _putchar, followed by a new line.
+*
+* Return: 0 (success)
+*
+*/
+void print_alphabet(void)
+}
+	int x;
+
+	for (x = 97; x <= 122; x++)
+{
+		_putchar(x);
+}
+_putchar('\n');
+return (0);
+{
