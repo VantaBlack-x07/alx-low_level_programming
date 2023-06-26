ad636fec5d045bb58a057bead05cf114f461bdba
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-26 08:42:22 -0700

    d

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
-* print_to_98 - function that prints all natural numbers
-* from n to 98, followed by a new line.
-*
-* @n: the number
-*
-* Return: Nothing
-*
-*/
-
+ * print_to_98 - function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ *
+ * @n: the number
+ *
+ * Return: Nothing
+ */
 void print_to_98(int n)
 {
-	if (n == 0 || n < 0)
+		int i;
+
+	if (n <= 98)
 	{
-		n = 1;
-	}
-	for (; n <= 98; n++)
-	{
-		_putchar(n + 48);
-		if (n != 98)
+		for (i = n; i < 98; i++)
 		{
-		_putchar(',');
-		_putchar(' ');
+			_putchar(i + 48);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar(i + 48);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar(98 + 48);
 }
