f13839f2dca8e1374198935f886c5e84d6a517f3
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-30 14:37:41 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,17 @@
 void rev_string(char *s)
 
 {
+	int x, y;
+	char temp;
 
+	for (x = 0 ; s[x] != '\0'; x++)
+	{
+	}
+	for (y = 0; y < x / 2; y++)
+	{
+		temp = s[y];
+		s[y] = s[i - 1 - y];
+		s[i - 1 - y] = temp;
+	}
 
 }
