81b321aea591b55a03bf548cd8a9027849ababd2
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-30 16:51:30 -0700

    d

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * puts_half - function that prints half of a string
@@ -11,5 +12,13 @@ void puts_half(char *str)
 
 {
 
+int x, len = strlen(str);
+char y;
+
+for (x = len / 2; x++)
+{
+	y = str[len - 1 - x];
+	_putchar(y);
+}
 
 }
