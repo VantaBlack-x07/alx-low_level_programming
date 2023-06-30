b96594dad710e8ce02dc3f92aaa85fc341284abd
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-06-30 13:25:27 -0700

    d

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -15,10 +15,7 @@ int _strlen(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != '\0')
-		{
-			c++;
-		}
+		c++;
 	}
 
 
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,7 +9,11 @@
 */
 
 void _puts(char *str)
-
 {
+	int x;
 
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
 }
