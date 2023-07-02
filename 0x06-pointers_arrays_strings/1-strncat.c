7834736ff49be9971fc3eebf709eec1e06307e00
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-02 16:32:23 -0700

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,27 @@
+#include "main.h"
+#include <string.h>
 
+/**
+* _strncat - function that concatenates two strings.
+*
+* @dest: destination string
+* @src: string to add to dest
+* @n: a number
+*
+* Return: pointer to the resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+char *dest_ptr = dest;
+int len = strlen(dest);
+int i;
+
+dest_ptr = dest;
+for (i = 0; i <= n; i++)
+{
+	dest_ptr[i + n] = src[i];
+}
+
+return (dest_ptr);
+}
