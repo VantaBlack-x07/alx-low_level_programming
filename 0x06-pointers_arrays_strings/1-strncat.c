7dfc70626aae9d48a6b6d107375cbf1e35b245e3
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-02 16:50:28 -0700

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-char *dest_ptr = dest;
 int len = strlen(dest);
 int i;
 
-*dest_ptr = dest;
-for (i = 0; i <= n; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-	dest_ptr[i + n] = src[i];
+	dest[i + len] = src[i];
 }
+dest[len + i] = '\0';
 
-return (dest_ptr);
+return (dest);
 }
