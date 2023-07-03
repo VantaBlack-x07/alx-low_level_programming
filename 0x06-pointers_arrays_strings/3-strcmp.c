793020d9e838705ff71a873cd09cd83b10ee2add
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-02 17:48:32 -0700

    d

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * strcmp - function that compares two strings
+ * _strcmp - function that compares two strings
+ *
  * @s1: pointer to first string
  * @s2: pointer to second string
  *
- * Return: 0 if the strings are equal,
- *	 a negative integer if s1 is less than s2,
- *	 or a positive integer if s1 is greater than s2
+ * Return:	0 if the strings are equal,
+ *		a negative integer if s1 is less than s2,
+ *		or a positive integer if s1 is greater than s2
  */
-int strcmp(char *s1, char *s2)
-{
-int i;
 
-for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+int _strcmp(char *s1, char *s2)
 {
-	if (s1[i] == s2[i])
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-}
 
-return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
