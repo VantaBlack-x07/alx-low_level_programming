a7f49f079a70981a018a01531b2f2427c1fc34e7
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-02 17:00:55 -0700

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,23 @@
+#include <string.h>
 
+/**
+ * _strncpy - function that copies a string
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of characters to concatenate
+ *
+ * Return: pointer to destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+
+for (x = 0; x < n && src[x] != '\0'; x++)
+{
+	dest[x] = src[x]
+}
+dest[x] = '\0';
+
+return (dest);
+}
