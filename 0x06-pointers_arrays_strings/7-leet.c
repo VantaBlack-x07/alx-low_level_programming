96771187fd2ed35a1971731dcc946734eca8b4bd
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-03 17:05:15 -0700

    d

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@
 char *leet(char *src)
 {
 	int i;
-	char alphabets[] = {'4', 'b', 'c', 'd', '3', 'f', 'g', 'h', 'i',
-	 'j', 'k', '1', 'm', 'n', '0', 'p', 'q', 'r',
-	  's', '7', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char lowercase_alphabets[] = {'4', 'b', 'c', 'd', '3', 'f', 'g', 'h', 'i',
+	'j', 'k', '1', 'm', 'n', '0', 'p', 'q', 'r',
+	's', '7', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	char uppercase_alphabets[] = {'4', 'B', 'C', 'D', '3', 'F', 'G', 'H', 'I',
 	'J', 'K', '1', 'M', 'N', '0', 'P', 'Q', 'R', 'S', '7', 'U',
-	 'V', 'W', 'X', 'Y', 'Z'};
+	'V', 'W', 'X', 'Y', 'Z'};
 	i = 0;
 	while (src[i] != '\0')
 	{
