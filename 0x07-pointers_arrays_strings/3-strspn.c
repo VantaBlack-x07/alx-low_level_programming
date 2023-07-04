96afcff78fa78525cf1bb9f9b874e24e01f2f887
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-04 13:18:08 -0700

    d

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,28 @@
+#include "main.h"
 
-
+/**
+* _strspn - function that gets the length of a prefix substring.
+*
+* @s: the string
+* @accept: characters to be searched in s
+*
+* Return: number of bytes in the initial segment of s
+*
+*/
 unsigned int _strspn(char *s, char *accept)
 {
+int x, y, count = 0;
 
+for (x = 0; s[x] != '\0'; x++)
+{
+	for (y = 0; accept[y] != '\0'; y++)
+	{
+		if (s[x] == accept[y])
+		{
+			count++;
+			break;
+		}
+	}
+}
+return (count);
 }
