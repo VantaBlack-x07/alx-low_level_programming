da6264584d02f22be3e2863493fb1ca51ca44c63
ASSEM <assem.hifny.aboelhamd@gmail.com>
2023-07-04 12:28:17 -0700

    d

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
-* _strchr - function that locates a character in a string.
-*
-* @s: the string
-* @c: the character
-*
-* Return: pointer to the first occurrence of the character c in the string s
-*	NULL if the character is not found
-*/
-
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: the string
+ * @c: the character
+ *
+ * Return: pointer to the first occurrence of the character c in the string s
+ *		 NULL if the character is not found
+ */
 char *_strchr(char *s, char c)
 {
-unsigned int i;
-char *ptr = NULL;
+	unsigned int i;
+	char *ptr = NULL;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-	if (s[i] == c)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		ptr = &s[i];
-		break;
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
 	}
-}
-return (ptr);
+
+	return (ptr);
 }
