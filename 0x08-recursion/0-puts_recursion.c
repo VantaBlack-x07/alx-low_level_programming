8ff314c5d1074e5035612f93c62f6f4a6e03008b
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-06 16:15:15 -0700

    p

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function that prints a string,
+* followed by a new line.
+*
+* @s: the string
+*/
+
+void _puts_recursion(char *s)
+{
+if (s == '\0')
+{
+	return;
+}
+else
+	_putchar(s[0];
+	_putchar(_puts_recursion(s + 1));
+
+}
