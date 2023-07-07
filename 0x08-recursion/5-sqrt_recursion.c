9a25ad73f07c59ecf14e900e9a881fdc6dc72998
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-06 19:30:48 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,23 +19,26 @@ int sqrt_helper(int n, int low, int high)
 	else
 	{
 		int mid = (low + high) / 2;
-		int mid_sqr = mid * mid;
+		int sqr = mid * mid;
 
-		if (mid_sqr == n)
+		if (sqr == n)
 		{
 			return (mid);
 		}
-	else if (mid_sqr < n)
-	{
-		return (sqrt_helper(n, mid + 1, high));
-	}
-	else
-	{
-		return (sqrt_helper(n, low, mid - 1));
-	}
+		else if (sqr < n)
+		{
+			return (sqrt_helper(n, mid + 1, high));
+		}
+		else if (sqr > n && (mid - 1) * (mid - 1) < n)
+		{
+			return (mid - 1);
+		}
+		else
+		{
+			return (sqrt_helper(n, low, mid - 1));
+		}
 	}
 }
-
 /**
 * _sqrt_recursion - function that returns
 * the natural square root of a number.
