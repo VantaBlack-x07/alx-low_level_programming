5445849eda53f1f9ae0b1119bdf96a5036f996c8
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-07 14:35:24 -0700

    fix

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,20 @@
-void _puts(char *s);
+#include "main.h"
+
+
+/**
+* _puts - function that prints a string, followed by a new line.
+*
+* @str: is a parameter.
+*
+*/
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,16 @@
-int _putchar(char c);
+#include <unistd.h>
+#include "main.h"
+
+/*
+* _putchar - writes the character c
+*
+* @c: char parameter
+*
+*  Return: 0 (success)
+*	  -1 (error)
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
