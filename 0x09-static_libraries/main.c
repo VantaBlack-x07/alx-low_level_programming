2c3f3711e32cab3bce9acfe579a09954a6a71666
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-07 17:13:05 -0700

    fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a quote by Kevin Mitnick and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
