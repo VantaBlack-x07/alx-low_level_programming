31bb26254f944948bc96a405d6e9957487fe7324
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-11 16:44:51 -0700

    FIX

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,21 +10,30 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = (s1 != NULL) ? strlen(s1) : 0;
-	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
+	unsigned int len1;
+	unsigned int len2;
 	char *arr;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	arr = malloc(len1 + len2 + 1);
-	if (arr == NULL)
-		return (NULL);
 
-	if (s1 != NULL)
-		strcpy(arr, s1);
-	if (s2 != NULL)
-		strcpy(arr + len1, s2);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(arr, s1);
+	strcpy(arr + len1, s2);
 
 	return (arr);
 }
