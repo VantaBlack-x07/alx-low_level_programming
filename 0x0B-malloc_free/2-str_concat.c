3a43e1e05d80f691c65b238c43e2c9c92319f4e3
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-11 16:36:27 -0700

    FIX

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,29 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * str_concat - function that concatenates two strings.
- *
- * @s1: first string to be concated
- * @s2: second string to be concated
- *
- * Return: pointer to new concated string OR NULL if it fails
- */
-
+* str_concat - concatenates two strings
+* @s1: first string to concatenate
+* @s2: second string to concatenate
+*
+* Return: pointer to concatenated string, or NULL on failure
+*/
 char *str_concat(char *s1, char *s2)
 {
-	char *arr;
-	int len = strlen(s1) + strlen(s2) + 1;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	arr = malloc(strlen(s1) + strlen(s2) + 1);
-
-	if (arr == NULL)
-	{
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	}
-	strcpy(arr, s1);
-	strcat(arr, s2);
-	arr[len - 1] = '\0';
+
+	unsigned int len1 = (s1 != NULL) ? strlen(s1) : 0;
+	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
+
+	char *arr = malloc(len1 + len2 + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	if (s1 != NULL)
+		strcpy(arr, s1);
+	if (s2 != NULL)
+		strcpy(arr + len1, s2);
 
 	return (arr);
 }
-
