16b6e625a85adb353ee97ebd9a709a8ac34f6f7c
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-19 16:30:06 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,11 +14,8 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL)
+	for (size_t x = 0; x < size; x++)
 	{
-		for (size_t x = 0; x < size; x++)
-		{
-			action(array[x]);
-		}
+		action(array[x]);
 	}
 }
