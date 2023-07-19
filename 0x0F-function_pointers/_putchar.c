d8f52066ced58c64660d85ef46c022303737b7c1
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-19 16:17:12 -0700

    fix

--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-#include "main.h"
 
-/*
-* _putchar - writes the character c
-*
-* @c: char parameter
-*
-*  Return: 0 (success)
-*	  -1 (error)
-*/
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _putchar(char c)
 {
