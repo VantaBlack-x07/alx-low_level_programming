8fab0e40e49d967c12d1f8b3f0ef7413bf210358
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-19 16:07:46 -0700

    init commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
 * print_name - function that prints a name.
 *
 * @name: name to be printed
-*
+* @f: pointer to a function
 */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f != NULL)
 	f(name);
 }
