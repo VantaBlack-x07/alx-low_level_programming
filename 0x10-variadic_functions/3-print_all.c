2561afc9b3d4ee4d0728267a84d650704058bd57
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-20 16:04:47 -0700

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,51 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
- * print_all - function that prints anything.
- * @format: a string of format
- * Return: Nothing
+ * print_all - prints any combination of c, i, f, and s format specifiers
+ * @format: a string of format specifiers
+ *
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	float f;
-	char *s, c;
 	va_list args;
+	int i = 0;
+	char *s;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
+		if (i > 0 && (format[i - 1] == 'c' || format[i - 1] == 'i'
+				|| format[i - 1] == 'f' || format[i - 1] == 's'))
+			printf(", ");
+
 		switch (format[i])
 		{
-			case 'f':
-				f = (float) va_arg(args, double);
-				printf("%f", f);
-				break;
 			case 'c':
-				c = (char) va_arg(args, int);
-				printf("%c", c);
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(args, double));
 				break;
 			case 's':
 				s = va_arg(args, char *);
 				if (s == NULL)
 					printf("(nil)");
 				else
-				{
 					printf("%s", s);
-				}
-				break;
-			case 'i':
-				i = va_arg(args, int);
-				printf("%d", i);
 				break;
 			default:
 				break;
 		}
-		if (format[i + 1] != '\0' && (format[i] == 'f'
-		  || format[i] == 'c' || format[i] == 's' || format[i] == 'i'))
-			printf(", ");
 		i++;
 	}
+
 	printf("\n");
 	va_end(args);
 }
