1d1a54b6fb34a1e39946b73e4842854773be61df
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-20 15:49:34 -0700

    init commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - function that prints anything.
+ *
+ * @format: a string of format
+ *
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	float f;
+	char *s, c;
+	va_list args;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		case 'f':
+		f = (float)va_arg(args, double);
+		printf("%f", f);
+		break;
+		case 'c':
+		c = va_arg(args, char);
+		printf("%c", c);
+		break;
+		case 's':
+		s = va_arg(args, char *)
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		break;
+		case 'i':
+		i = va_arg(args, int);
+		printf("%d", i);
+		break;
+		default:
+		break;
+		if (format[i + 1] != '\0' && format[i] == 'f'
+		|| format[i] == 'c' || format[i] == 's' || format[i] == 'i')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
