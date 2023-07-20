870916002ab0f5211a923caf6a37da771daf7496
Assem <assem.hifny.aboelhamd@gmail.com>
2023-07-19 17:51:07 -0700

    FIX

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,17 +2,13 @@
 
 /**
  * print_name - prints a name.
- *
- * @name: name to be printed
+ * @name: char pointer to name
  * @f: funtcion pointer
- *
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	{
 		f(name);
-	}
 }
